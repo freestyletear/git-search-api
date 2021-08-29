150f608dc68d8b32b525a5a4bb5e808634798c3b
freestyletear <freestyletear@gmail.com>
2021-08-29 14:07:11 +0300

    add boilerplates

--- git-search-api/src/app/app.module.ts
+++ git-search-api/src/app/app.module.ts
@@ -1,11 +1,16 @@
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { RepoComponent } from './components/repo/repo.component';
+import { UserComponent } from './components/user/user.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RepoComponent,
+    UserComponent,
+    SearchComponent,
     
   ],
   imports: [

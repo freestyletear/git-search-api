61bc82b764f8493ea6ce92cbe2d0dbf76210353e
freestyletear <freestyletear@gmail.com>
2021-08-29 13:52:57 +0300

    redo for correction

--- git-search-api/src/app/app.module.ts
+++ git-search-api/src/app/app.module.ts
@@ -1,30 +1,18 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http' /* I ADDED THIS */
-/* ON TOP HERE WAS import { HttpModule } from '@angular/http'; */
-
-import { FormsModule } from '@angular/forms';
-
-import { ProfileService } from './services/profile.service';
-
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent,
     
   ],
   imports: [
     BrowserModule,
-    /*THIS WAS JUST HERE HttpModule,*/
-    FormsModule,
-    HttpClientModule /* I ADDED THIS */
 
   ],
-  providers: [ProfileService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
